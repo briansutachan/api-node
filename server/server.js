eb16b1a01e8de2d10edda36250deb405d20d37e1
briansutachan <bsutachan@techlaunch.io>
2018-07-26 17:35:30 -0400

    server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,52 @@
 var express = require('express');
 var app = express();
-var port = 3000;
-var host = '173.109.109.66';
+var _ = require('lodash');
+const port = 3050;
+const host = '173.109.109.66';
 
-var jsonData = { count: 12, message: 'hey' };
+app.use(express.static('client'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.get('/', (requestAnimationFrame, res) => {
-    //res sendFile takes on absolute path to a file and sets the mime type based on the file extension
-    res.sendFile(dir__dirname + '/index.html', err => {
-        if (err) {
-            res.status(500).send(err);
-        }
-    })
-})
+var lions = [
+    {
+    id : 1,
+    name : 'Simba',
+    pride : 'The cool cats',
+    age : 3,
+    gender : 'male'
+},
+{
+    id : 2,
+    name : 'Mufasa',
+    pride : 'The cool cats',
+    age : 4,
+    gender : 'male' 
+}];
+var id = 0;
 
-app.listen(port, host, function() {
-console.log('Listening on http://localhost', port)
+app.get('/lions', (req, res) => {
+    res.json(lions);
 });
+
+app.get('/lions/:id', (req, res) => {
+    var lion = lions.find(lion => {
+        return lion.id == req.params.id;
+    });
+
+    res.json(lion || {});
+});
+
+app.post('/lions', (req, res) => {
+    var lion = req.body;
+    id++;
+    lion.id = id + '';
+
+    lions.push(lion);
+
+    res.json(lion)
+});
+
+app.listen(port, host, function () {
+    console.log('Listening on http://localhost:', port);
+})
