d81f58c83689c1b93a88a7bb76b8cbf844d43d9a
briansutachan <bsutachan@techlaunch.io>
2018-07-26 21:39:49 -0400

    Completed refactoring for ID and parents

--- server/server.js
+++ server/server.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
-var _ = require('lodash');
+var morgan = require('morgan');
 const port = 3050;
 const host = '127.0.0.1';
-
-app.use(express.static('client'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 var lions = [
     {
@@ -22,26 +18,49 @@ var lions = [
         pride: 'The cool csats',
         age: 4,
         gender: 'male'
-    }];
-var id = 0;
+    }
+];
+var id = 2;
+
+var updateId = function (req, res, next) {
+    if (!req.body.id) {
+        id++;
+        req.body.id = id + '';
+    }
+
+    next();
+}
+
+app.user(morgan('dev'));
+app.use(express.static('client'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.param('id', (req, res, next, id) => {
+    var lion = lions.find(lion => {
+        return lion.id == id;
+    });
+    if(lion) {
+        req.lion = lion;
+        next();
+    }else{
+        res.send()
+    }
+});
 
 app.get('/lions', (req, res) => {
     res.json(lions);
 });
 
 app.get('/lions/:id', (req, res) => {
-    var lion = lions.find(lion => {
-        return lion.id == req.params.id;
-    });
+    var lion = req.lion;
 
     res.json(lion || {});
 });
 
-app.post('/lions', (req, res) => {
+app.post('/lions', updateId, (req, res) => {
     var lion = req.body;
-    id++;
-    lion.id = id + '';
-
+    
     lions.push(lion);
 
     res.json(lion)
@@ -56,7 +75,7 @@ app.put('/lions/:id', (req, res) => {
     var lion = lions.findIndex(lion => lion.id == req.params.id);
     if (!lions[lion]) {
         res.send();
-    }else{
+    } else {
         var updatedLion = Object.assign(lions[lion], update);
         //_.assign(lions[lion], update);
         res.json(updatedLion)
@@ -66,14 +85,20 @@ app.put('/lions/:id', (req, res) => {
 
 app.delete("/lions/:id", (req, res) => {
     var lion = lions.findIndex(lion => lion.id == req.params.id);
-    if(!lions[lion]) {
+    if (!lions[lion]) {
         res.send();
     } else {
         var deletedLion = lions[lion];
         lions.splice(lion, 1);
-        res,json(deletedLion);
+        res, json(deletedLion);
     }
 });
+
+app.use{(err, req, res, next) => {
+    if (err) {
+        res.status(500).send(err);
+    }
+}};
 
 app.listen(port, host, function () {
     console.log("Listening on http://localhost:", port);
